adfa626acc06bf2d03e9a1f5b6597aa69f7683ac
しょう <show@SK-MacBook-Air.local>
2025-06-07 16:36:18 +0900

    edit index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,23 +1,3 @@
-<!-- @extends('layouts.app')
-
-@section('content')
-    <h1>投稿一覧</h1>
-    <a href="{{ route('posts.create') }}">新規作成</a>
-    <ul>
-        @foreach ($posts as $post)
-            <li>
-                <a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a>
-                <a href="{{ route('posts.edit', $post->id) }}">編集</a>
-                <form action="{{ route('posts.destroy', $post->id) }}" method="POST" style="display:inline;">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">削除</button>
-                </form>
-            </li>
-        @endforeach
-    </ul>
-@endsection -->
-
 @extends('layouts.app')
 
 @section('content')
